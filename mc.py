01590fb12bea3ed58a4206b7a6d8e219dfb29a52
Patrick Emonts <patrick.emonts@mpq.mpg.de>
2021-06-01 09:55:10 +0200

    Add comments (and a bit of formatting)

--- mc.py
+++ mc.py
@@ -37,10 +37,10 @@ class MonteCarloManager:
             system_cfg_id = ray.put(self.system_cfg)
             for i in range(self.nrunner):
                 cfg = copy.deepcopy(self.mc_cfg)
-                cfg.seed = self.mc_cfg.seed+i
-                cfg.meas_steps = self.mc_cfg.meas_steps//self.nrunner
-                resultvec.append(run_mc.remote(
-                    i, cfg, self.system_cls, system_cfg_id))
+                cfg.seed = self.mc_cfg.seed + i
+                cfg.meas_steps = self.mc_cfg.meas_steps // self.nrunner
+                resultvec.append(
+                    run_mc.remote(i, cfg, self.system_cls, system_cfg_id))
             resultvec = ray.get(resultvec)
             return self.collect(resultvec)
         else:
@@ -55,20 +55,20 @@ class MonteCarloManager:
         system = self.system_cls(self.system_cfg)
         dest = MonteCarloEstimator(self.mc_cfg, system)
         if len(resultvec) > 1:
-            dest.obsdict = utils.mergeDict(
-                resultvec[0].obsdict, resultvec[1].obsdict)
+            dest.obsdict = utils.mergeDict(resultvec[0].obsdict,
+                                           resultvec[1].obsdict)
             for mc_runner in resultvec[2:]:
                 dest.obsdict = utils.mergeDict(dest.obsdict, mc_runner.obsdict)
         else:
             dest = resultvec[0]
         return dest
 
+
 ################################### Monte Carlo runner and config ###############
 
 
 class MonteCarloEstimatorConfig:
     """Monte Carlo Configuration"""
-
     def __init__(self):
         self.warmup_steps = None
         self._seed = None
@@ -102,31 +102,33 @@ class MonteCarloEstimator:
         self.step = 0
 
         #This might change in the future if we implement different updates
-        self.update=self.update_single_site
-
+        self.update = self.update_single_site
 
     def init_measurements(self):
         """Add empty measurement vectors to the measurement dictionary"""
         binsize = self.cfg.binsize
-        self.obsdict["acceptance_prob"] = Measurement(
-            "Acceptance Probablity", binsize)
+        self.obsdict["acceptance_prob"] = Measurement("Acceptance Probablity",
+                                                      binsize)
         self.obsdict["energy"] = Measurement("Energy", binsize)
         self.obsdict["mag_energy"] = Measurement("Magnetic Energy", binsize)
         self.obsdict["el_energy"] = Measurement("Electric Energy", binsize)
         self.obsdict["wilson_00_11"] = Measurement("Wilson (0,0) 1x1", binsize)
-        self.obsdict["polyakov_00_x"] = Measurement("Polyakov (0,0) x", binsize)
+        self.obsdict["polyakov_00_x"] = Measurement("Polyakov (0,0) x",
+                                                    binsize)
         #self.obsdict["cov_ferm"] = Measurement("Covariance Matrix fermions", binsize)
 
     def measure(self):
         """Measure the corresponding observables in the dictionary"""
         polyakov_loop = self.system.cfg.lattice.generate_polyakov_loop(
             (0, 0), lattice.Direction.X)
-        wilson_loop = self.system.cfg.lattice.generate_wilson_loop(
-            (0, 0), (1,1))
+        wilson_loop = self.system.cfg.lattice.generate_wilson_loop((0, 0),
+                                                                   (1, 1))
 
         self.obsdict["energy"].append(self.system.energy)
-        self.obsdict["wilson_00_11"].append(np.real(self.system.compute_path(wilson_loop)))
-        self.obsdict["polyakov_00_x"].append(np.real(self.system.compute_path(polyakov_loop)))
+        self.obsdict["wilson_00_11"].append(
+            np.real(self.system.compute_path(wilson_loop)))
+        self.obsdict["polyakov_00_x"].append(
+            np.real(self.system.compute_path(polyakov_loop)))
         #self.obsdict["cov_ferm"].append(self.system.compute_ferm_cov())
         self.obsdict["mag_energy"].append(self.system.mag_energy)
         self.obsdict["el_energy"].append(self.system.el_energy)
@@ -141,7 +143,7 @@ class MonteCarloEstimator:
 
     def run(self):
         """Meaurement phase phase (with measurement)"""
-        while self.step < self.cfg.warmup_steps+self.cfg.meas_steps:
+        while self.step < self.cfg.warmup_steps + self.cfg.meas_steps:
             if self.step % 1000 == 0:
                 logging.debug("Run: {}".format(self.step))
             self.update()
@@ -149,24 +151,30 @@ class MonteCarloEstimator:
             self.step += 1
 
     def update_single_site(self):
+        """Update for the MC simulation.
+        This updates randomly chooses a single site and updates it.
+        The update is local. The new gauge field value is drawn uniformly from the distribution of possible gauge fields (according to the gauge group).
+        """
         # Pick a site to update
-        lattice=self.system.cfg.lattice
-        nlinks=lattice.nlinks
-        link_ind=np.random.randint(0,nlinks)
+        lattice = self.system.cfg.lattice
+        nlinks = lattice.nlinks
+        link_ind = np.random.randint(0, nlinks)
         # Uniformly pick a gauge to replace
-        theta=self.system.gaugemgr.get_random_gauge_value()
+        theta = self.system.gaugemgr.get_random_gauge_value()
         # Store the old values
-        weight_old=self.system.weight
-        weight_new=self.system.calculate_weight_attempt(link_ind,theta)
+        weight_old = self.system.weight
+        weight_new = self.system.calculate_weight_attempt(link_ind, theta)
         if np.exp(weight_new - weight_old) > np.random.rand():
             # Accept
             self.obsdict["acceptance_prob"].append(1)
-            self.system.update_gauge_ind(link_ind,theta)
+            self.system.update_gauge_ind(link_ind, theta)
         else:
             # Reject
             self.obsdict["acceptance_prob"].append(0)
 
     def simulate(self):
+        """Main routine to start a Monte Carlo simulation.
+        """
         self.warmup()
         self.run()
 
@@ -214,16 +222,18 @@ class MonteCarloEstimator:
             pickle.dump(data_full, outfile)
 
     def save(self):
-        syscfg=self.system.cfg
-        meas_steps=self.cfg.meas_steps
-        warmup_steps=self.cfg.warmup_steps
-        t=syscfg.paramdict["t"]
-        y=syscfg.paramdict["y"]
-        z=syscfg.paramdict["z"]
+        syscfg = self.system.cfg
+        meas_steps = self.cfg.meas_steps
+        warmup_steps = self.cfg.warmup_steps
+        t = syscfg.paramdict["t"]
+        y = syscfg.paramdict["y"]
+        z = syscfg.paramdict["z"]
         fname_full = "data_L_{:02d}_gel_{:.3f}_gm_{:.3f}_gmag_{:.3f}_t_{:.3f}_y_{:.3f}_z_{:.3f}_wsteps_{:07d}_msteps_{:07d}.pkl.gz".format(
-            syscfg.lattice.nx, syscfg.g_el, syscfg.g_gm, syscfg.g_mag, t, y, z, warmup_steps, meas_steps)
+            syscfg.lattice.nx, syscfg.g_el, syscfg.g_gm, syscfg.g_mag, t, y, z,
+            warmup_steps, meas_steps)
         fname_summary = "summary_L_{:02d}_gel_{:.3f}_gm_{:.3f}_gmag_{:.3f}_t_{:.3f}_y_{:.3f}_z_{:.3f}_wsteps_{:07d}_msteps_{:07d}.pkl".format(
-            syscfg.lattice.nx, syscfg.g_el, syscfg.g_gm, syscfg.g_mag, t, y, z, warmup_steps, meas_steps)
+            syscfg.lattice.nx, syscfg.g_el, syscfg.g_gm, syscfg.g_mag, t, y, z,
+            warmup_steps, meas_steps)
         self.save_full(fname_full)
         self.save_summary(fname_summary)
 
@@ -240,8 +250,20 @@ class MonteCarloEstimator:
                 print("<{}>".format(key), self.obsdict[key].mean())
 
     def summary(self):
-        dest = {"name": [], "t": [], "y": [], "z": [], "g_el": [], "g_gm": [], "g_mag": [], "warmup_steps": [
-        ], "meas_steps": [], "seed": [], "mean": [], "err": []}
+        dest = {
+            "name": [],
+            "t": [],
+            "y": [],
+            "z": [],
+            "g_el": [],
+            "g_gm": [],
+            "g_mag": [],
+            "warmup_steps": [],
+            "meas_steps": [],
+            "seed": [],
+            "mean": [],
+            "err": []
+        }
         for key in self.obsdict.keys():
             dest['name'].append(key)
             dest['g_el'].append(self.system.cfg.g_el)
