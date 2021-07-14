Fix mode ordering bug in gamma_maj_sys derivatives

system.py
@@ -275,6 +275,25 @@ class Z2System2D:
             self._gamma_maj = np.real(smat@gamma_dirac@np.transpose(smat))
         return self._gamma_maj
 
+    def _expand_gamma_maj_to_system(self,covmat):
+        permbuilder = lat.PermutationBuilderGMS2D(self.cfg.lattice, nmodes_per_link=1)
+        mat_perm = permbuilder.perm()
+        nsites=self.cfg.lattice.size
+        id = np.eye(nsites)
+        # Extract the parts of the covariance matrix
+        amat = covmat[:2, :2]
+        bmat = covmat[:2, 2:]
+        dmat = covmat[2:, 2:]
+        #Expand them
+        amat_sys = np.kron(id, amat)
+        bmat_sys = np.kron(id, bmat)
+        dmat_sys = np.kron(id, dmat)
+        #Reassemble them in the correct order
+        mat_sys_unordered= np.block(
+            [[amat_sys, bmat_sys], [-np.transpose(bmat_sys), dmat_sys]])
+        dest=mat_perm@mat_sys_unordered@np.transpose(mat_perm)
+        return dest
+
     @property
     def gamma_maj_sys(self):
         """Return the covariance matrix of the full system in Majorana modes.
@@ -285,21 +304,7 @@ class Z2System2D:
             [np.array]: Covariance matrix of the full system
         """
         if self._gamma_maj_sys is None:
-            gamma_maj = self.gamma_maj
-            amat = gamma_maj[:2, :2]
-            bmat = gamma_maj[:2, 2:]
-            dmat = gamma_maj[2:, 2:]
-            nsites = self.cfg.lattice.size
-            id = np.eye(nsites)
-            # Extract the parts of the covariance matrix
-            amat_sys = np.kron(id, amat)
-            bmat_sys = np.kron(id, bmat)
-            dmat_sys = np.kron(id, dmat)
-            # Order the modes of the covariance matrix according to gamma_in_sys
-            permbuilder = lat.PermutationBuilderGMS2D(self.cfg.lattice, nmodes_per_link=1)
-            mat_perm = permbuilder.perm()
-            self._gamma_maj_sys = mat_perm@np.block(
-                [[amat_sys, bmat_sys], [-np.transpose(bmat_sys), dmat_sys]])@np.transpose(mat_perm)
+            self._gamma_maj_sys=self._expand_gamma_maj_to_system(self.gamma_maj)
         return self._gamma_maj_sys
 
     def initialize_gamma_in_sys(self):
@@ -570,37 +575,23 @@ class Z2System2D:
         return dest
             
 
+
     @property
     def gamma_maj_sys_deriv_y(self):
         if self._gamma_maj_sys_deriv_y is None:
-            permbuilder = lat.PermutationBuilderGMS2D(self.cfg.lattice, nmodes_per_link=1)
-            mat_perm = permbuilder.perm()
-            gamma_maj_deriv_y = self.compute_gamma_maj_deriv_y()
-            nsites=self.cfg.lattice.size
-            gamma_maj_deriv_y_sys_unsorted=np.kron(np.eye(nsites),gamma_maj_deriv_y)
-            self._gamma_maj_sys_deriv_y=mat_perm@gamma_maj_deriv_y_sys_unsorted@np.transpose(mat_perm)
+            self._gamma_maj_sys_deriv_y=self._expand_gamma_maj_to_system(self.compute_gamma_maj_deriv_y())
         return self._gamma_maj_sys_deriv_y
 
     @property
     def gamma_maj_sys_deriv_z(self):
         if self._gamma_maj_sys_deriv_z is None:
-            permbuilder = lat.PermutationBuilderGMS2D(self.cfg.lattice, nmodes_per_link=1)
-            mat_perm = permbuilder.perm()
-            gamma_maj_deriv_z = self.compute_gamma_maj_deriv_z()
-            nsites=self.cfg.lattice.size
-            gamma_maj_deriv_z_sys_unsorted=np.kron(np.eye(nsites),gamma_maj_deriv_z)
-            self._gamma_maj_sys_deriv_z=mat_perm@gamma_maj_deriv_z_sys_unsorted@np.transpose(mat_perm)
+            self._gamma_maj_sys_deriv_z=self._expand_gamma_maj_to_system(self.compute_gamma_maj_deriv_z())
         return self._gamma_maj_sys_deriv_z
 
     @property
     def gamma_maj_sys_deriv_t(self):
         if self._gamma_maj_sys_deriv_t is None:
-            permbuilder = lat.PermutationBuilderGMS2D(self.cfg.lattice, nmodes_per_link=1)
-            mat_perm = permbuilder.perm()
-            gamma_maj_deriv_t = self.compute_gamma_maj_deriv_t()
-            nsites=self.cfg.lattice.size
-            gamma_maj_deriv_t_sys_unsorted=np.kron(np.eye(nsites),gamma_maj_deriv_t)
-            self._gamma_maj_sys_deriv_t=mat_perm@gamma_maj_deriv_t_sys_unsorted@np.transpose(mat_perm)
+            self._gamma_maj_sys_deriv_t=self._expand_gamma_maj_to_system(self.compute_gamma_maj_deriv_t())
         return self._gamma_maj_sys_deriv_t
 
     def compute_gamma_maj_deriv_y(self):

test.py
@@ -487,6 +487,42 @@ class TestZ2SystemMethods(unittest.TestCase):
         res = system_z2_2_2.gamma_maj_sys_deriv("t")
         self.assertTrue(utils.is_antisymmetric(res))
 
+    def test_derivative_gamma_sys_finite_diff(self):
+        # This is comparison of the analytic derivative against the numeric derivative
+        # There is no sampling involved here. The gauge field is 0.
+        eps=1e-5
+        lat = lattice.Lattice2D(2, 2)
+        t=0.2
+        y=0.3
+        z=0.8
+        paramdict = {"y": y, "z": z, "t": t}
+        param_namevec=["t","y","z"]
+        lat_2x2 = lattice.Lattice2D(2, 2)
+        system_cfg = system.Z2System2DConfig(paramdict, lat_2x2, 1.0, None, None)
+        for ind in range(3):
+            with self.subTest(ind=ind):
+                paramvec=system_cfg.paramvec
+                paramvec_left=np.copy(paramvec)
+                paramvec_right=np.copy(paramvec)
+                paramvec_left[ind]-=eps
+                paramvec_right[ind]+=eps
+                system_cfg_left = system.Z2System2DConfig(paramvec_left, lat_2x2, 1.0,
+                                                        None, None)
+                system_cfg_right = system.Z2System2DConfig(paramvec_right, lat_2x2,
+                                                        1.0, None, None)
+
+                system_z2_2_2 = system.Z2System2D(system_cfg)
+                system_z2_2_2_left= system.Z2System2D(system_cfg_left)
+                system_z2_2_2_right = system.Z2System2D(system_cfg_right)
+
+                deriv_maj_sys=system_z2_2_2.gamma_maj_sys_deriv(param_namevec[ind])
+                deriv_maj_sys_left=system_z2_2_2_left.gamma_maj_sys
+                deriv_maj_sys_right=system_z2_2_2_right.gamma_maj_sys
+
+                deriv_maj_sys_num=(deriv_maj_sys_right-deriv_maj_sys_left)/(2*eps)
+
+                self.assertTrue(np.allclose(deriv_maj_sys_num, deriv_maj_sys))
+        
     def test_norm_minimal(self):
         # This update is a nullop since we initialize the gauge-field with 0
         zeroarr = np.zeros((1, 1))
@@ -559,11 +595,10 @@ class TestZ2SystemMethods(unittest.TestCase):
         z=0.56
         paramdict = {"y": y, "z": z, "t": t}
         param_namevec=["t","y","z"]
+        lat_2x2 = lattice.Lattice2D(2, 2)
+        system_cfg = system.Z2System2DConfig(paramdict, lat_2x2, 1.0, None, None)
         for ind in range(3):
             with self.subTest(ind=ind):
-                lat_2x2 = lattice.Lattice2D(2, 2)
-                system_cfg = system.Z2System2DConfig(paramdict, lat_2x2, 1.0, None,
-                                                    None)
                 paramvec=system_cfg.paramvec
                 paramvec_left=np.copy(paramvec)
                 paramvec_right=np.copy(paramvec)
@@ -705,7 +740,7 @@ class TestMinimizerZ2(unittest.TestCase):
 
     def test_derivative_el_energy_exact(self):
         eps = 1e-5
-        paramdict = {"t": 0.0, "y": 0.5, "z": 0.5}
+        paramdict = {"t": 0.2, "y": 0.5, "z": 0.5}
         for ind in range(3):
             with self.subTest(ind=ind):
                 lat_2x2 = lattice.Lattice2D(2, 2)