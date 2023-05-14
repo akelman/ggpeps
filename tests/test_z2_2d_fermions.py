155c31d470b8ea4009e74314b9ac6e65cba2dc01
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2023-05-14 14:52:52 +0300

    Add tests for ansatz with fermions.

--- tests/test_z2_2d_fermions.py
+++ tests/test_z2_2d_fermions.py
@@ -4,9 +4,11 @@ import numpy as np
 
 from ggpeps import lattice
 from ggpeps.lattice import Direction
-from ggpeps import system
+from ggpeps import system, exacteval
 from ggpeps import utils
 from ggpeps.mc import MonteCarloEstimatorConfig, MonteCarloEstimator, MonteCarloManager
+from ggpeps.system import Z2System2D4C_Config, Z2System2D4C
+from ggpeps.minimizer import Minimizer, MinimizerConfig
 from ggpeps.utils import compare_array_elementwise
 
 # ======================= Z2 fermionic system (4 copies) =========================================
@@ -16,7 +18,7 @@ class TestZ2C4System(unittest.TestCase):
 
         lat = lattice.Lattice2D(2,2)
         paramvec = np.random.rand(2, 20)
-        cfg = system.Z2System2D4C_Config(lat, 0, 0, 0, 0)
+        cfg = system.Z2System2D4C_Config(lat, 1,1,1,1)
         cfg.paramvec = paramvec
         self.system_z2 = system.Z2System2D4C(cfg) 
         self.system_z2.cfg.enforce_parameter_conditions(self.system_z2.cfg.paramvec)   
@@ -39,10 +41,31 @@ class TestZ2C4System(unittest.TestCase):
         self.assertTrue(np.allclose(covmat_layer1, expected_covmat))
         self.assertTrue(np.allclose(covmat_layer2, expected_covmat))
 
+    def test_t_zero(self):
+        """Ensure mass and interaction energy are zero when t = 0"""
+
+        self.system_z2.cfg.make_pure_gauge() # sets t params to zero
+        ex_eval = exacteval.ExactEvaluator(self.system_z2)
+        dest_dict = ex_eval.evaluate()
+        self.assertTrue(np.allclose(0, dest_dict['mass_energy']))
+        self.assertTrue(np.allclose(0, dest_dict['int_energy']))
+
+    def test_t_nonzero(self):
+        """Ensure mass and interaction energy are zero when t != 0. 
+        This checks for random params, which we assume do not give t = 0"""
+        
+        ex_eval = exacteval.ExactEvaluator(self.system_z2)
+        dest_dict = ex_eval.evaluate()
+        self.assertFalse(np.allclose(0, dest_dict['mass_energy']))
+        self.assertFalse(np.allclose(0, dest_dict['int_energy']))
+
+  
+    def test_free_fermions_gs_energy(self):
+        """Ensure gs energy for free fermion case matches the analytic result"""
+        pass
+
     # TESTS TO ADD
     # get the correct gamma_in_sys for all layers
-    # mass energy is zero when t = 0
-    # int energy is zero when t = 0
     # when interaction is off, ground state is: no fermions, pure-gauge ground state
     # ensure covmat is not the no-fermions one in cases where t != 0
     # required parameters are zero (i.e. the ones that are zero by def of the ansatz) - before starting, and remain that way through minimization
