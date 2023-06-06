2e838300395eaf9fe87e06be810d0d0a088c16d9
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2023-06-06 13:46:11 +0300

    Update argparse help messages.

--- manager.py
+++ manager.py
@@ -376,9 +376,9 @@ if __name__ == "__main__":
     
     # Arguments for the minimizer
     parser.add_argument("--method", type=str, default="bfgs", help="Minimization method")
-    parser.add_argument("--maxiter", type=int, default=100, help="Number of steps for the minimizer")
+    parser.add_argument("--maxiter", type=int, default=100, help="Maximum number of steps for the minimizer")
     parser.add_argument("--alpha", "--lr", type=float, default=0.1, help="Learning rate")
-    parser.add_argument("--min-grad", type=float, default=1e-5, help="Minimal gradient to use a stopping criterion")
+    parser.add_argument("--min-grad", type=float, default=1e-5, help="Minimal gradient to use as a stopping criterion")
     
     # Arguments for ray
     parser.add_argument("--nrunner", type=int, default=0, help="Number of parallel MC runners")
