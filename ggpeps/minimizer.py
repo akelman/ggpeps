90475dcb6676a3728e2e18e215586b546d9d2a08
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2023-06-20 13:07:46 +0300

    Fix typo (underscore rather than dash)

--- ggpeps/minimizer.py
+++ ggpeps/minimizer.py
@@ -42,7 +42,7 @@ class MinimizerConfig():
         self._method = val.upper()
 
 class Minimizer():
-    supported_methods = ["CG", "BFGS", "L-BFGS-B", "POWELL", "NELDER_MEAD", "TNC", "TRUST_NCG"]
+    supported_methods = ["CG", "BFGS", "L-BFGS-B", "POWELL", "NELDER-MEAD", "TNC", "TRUST-NCG"]
 
     def __init__(self, cfg, evaluator, use_exact=False):
         self.cfg = cfg
