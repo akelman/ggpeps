26866dbaeaea2f95172bd7df48fed733ff79a8ed
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2023-08-11 12:27:21 +0300

    Remove requirement that ED data always be orange.

--- plotter.py
+++ plotter.py
@@ -40,7 +40,7 @@ def main(args):
             yvals = yvals_dict[ob]
             xvals_m, yvals_m = zip(*sorted(zip(xvals, yvals))) # we define new variables so as not to change xvals while looping over the observables
 
-            ax.plot(xvals_m, yvals_m, label = f"ED, obs={ob}", c="orange")
+            ax.plot(xvals_m, yvals_m, label = f"ED, obs={ob}")
 
     if args.ec is not None:
         for ind, ec_data in enumerate(args.ec):
