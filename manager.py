4ff47a9f167fa9ce1eccae5146b5e13328e75b9f
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2023-09-13 15:51:13 +0300

    Clarify comment

--- manager.py
+++ manager.py
@@ -214,7 +214,7 @@ def main(args):
     paramvec = translate_parameters(system_cfg, args.params, rngstate)
     system_cfg.paramvec = paramvec
 
-    # Ensure pure gauge (setting t parameter to zero) if enabled
+    # Ensure pure gauge (setting t parameters to zero) if enabled
     if not args.fermions:
         system_cfg.make_pure_gauge()
 
