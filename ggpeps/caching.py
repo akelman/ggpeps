5cabef4cc998f8815b7c5b96dfc191b8cd22ac19
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-01-15 16:11:18 +0200

    Move caching to separate file Fix Evaluator -> EvaluatorManager bug in print_callback()

---
+++ ggpeps/caching.py
@@ -0,0 +1,77 @@
+import os
+import pickle
+import numpy as np
+
+from ggpeps import utils
+from ggpeps import logger
+
+####################### Caching #######################
+
+class Cache:
+    def __init__(self, cache_file: str = 'cache.pkl'):
+        self.cache_version = 0.1
+        self.cache_file: str = cache_file
+        self.cache_data: dict = {'cache_version': self.cache_version,
+                                 'git_hash': utils.get_git_hash(),
+                                 'minimizer': None,
+                                 'evaluator_manager': None,
+                                 'system': None,
+                                 'energy': {}, 
+                                 'energy_grad': {}} 
+
+    def paramvec2key(self, paramvec: np.ndarray):
+        return paramvec.data.tobytes()
+
+    def key2paramvec(self, key: bytes):
+        return np.frombuffer(key)
+
+    def save_cache_file(self):
+        with open(self.cache_file, "wb") as outfile:
+            pickle.dump(self.cache_data, outfile)
+
+    def add_obj_to_cache(self, obj_name: str, obj_val):
+        if obj_name not in self.cache_data.keys():
+            logger.warn(f"Cache does not support {obj_name}. Not adding to cache.")
+        else:
+            self.cache_data[obj_name] = obj_val
+        return
+
+    def load_obj_from_local_cache(self, obj_name: str):
+        return self.cache_data[obj_name]
+    
+    def add_obs_to_cache(self, paramvec: np.ndarray, obs: str, val: float, save_to_file: bool = True):
+        key = self.paramvec2key(paramvec)
+        obs_cache = self.cache_data[obs]
+        obs_cache[key] = val
+
+        obs_cache_len = len(obs_cache)
+        if obs_cache_len > 1000 and obs_cache_len % 50: # 1000 is an arbitrary threshold
+            logger.warn(f"Cache for obs {obs} is large: {obs_cache_len} items.")
+
+        # Save to pickle file
+        if save_to_file:
+            self.save_cache_file()
+
+    def load_obs_from_local_cache(self, paramvec: np.ndarray, obs: str):
+        if obs not in ['energy', 'energy_grad']:
+            raise ValueError(f"Unknown observable {obs} is not in cache.")
+        obs_cache = self.cache_data[obs]
+        for key in obs_cache.keys():
+            if np.allclose(self.key2paramvec(key), paramvec):
+                return obs_cache[key]
+        return None
+
+    def load_cache_file(self, cache_file: str):
+        # TODO: once we include other objects in the cache,
+        #       this function should check that cached objects have the same configs
+        if os.path.exists(cache_file):
+            with open(cache_file, "rb") as infile:
+                cache_data = pickle.load(infile)
+                if cache_data['cache_version'] == self.cache_version:
+                    self.cache_data = cache_data
+                    logger.info(f"Loaded cache file {cache_file}")
+                else:
+                    logger.warn(f"Cache file {cache_file} has version {cache_data['cache_version']} \
+                                but we are using version {self.cache_version}. Ignoring cached data.")
+                    # TODO: we can probably recover some of the data
+        return self.cache_data
--- ggpeps/minimizer.py
+++ ggpeps/minimizer.py
@@ -6,82 +6,10 @@ import pickle
 import numpy as np
 from scipy.optimize import minimize
 
-from ggpeps import utils
 from ggpeps import logger
+from ggpeps.caching import Cache
 from ggpeps.evaluator_manager import EvaluatorManager
 from ggpeps.evaluator import Evaluator
-
-
-####################### Caching #######################
-
-class Cache:
-    def __init__(self, cache_file: str = 'cache.pkl'):
-        self.cache_version = 0.1
-        self.cache_file: str = cache_file
-        self.cache_data: dict = {'cache_version': self.cache_version,
-                                 'git_hash': utils.get_git_hash(),
-                                 'minimizer': None,
-                                 'evaluator_manager': None,
-                                 'system': None,
-                                 'energy': {}, 
-                                 'energy_grad': {}} 
-
-    def paramvec2key(self, paramvec: np.ndarray):
-        return paramvec.data.tobytes()
-
-    def key2paramvec(self, key: bytes):
-        return np.frombuffer(key)
-
-    def save_cache_file(self):
-        with open(self.cache_file, "wb") as outfile:
-            pickle.dump(self.cache_data, outfile)
-
-    def add_obj_to_cache(self, obj_name: str, obj_val):
-        if obj_name not in self.cache_data.keys():
-            logger.warn(f"Cache does not support {obj_name}. Not adding to cache.")
-        else:
-            self.cache_data[obj_name] = obj_val
-        return
-
-    def load_obj_from_local_cache(self, obj_name: str):
-        return self.cache_data[obj_name]
-    
-    def add_obs_to_cache(self, paramvec: np.ndarray, obs: str, val: float, save_to_file: bool = True):
-        key = self.paramvec2key(paramvec)
-        obs_cache = self.cache_data[obs]
-        obs_cache[key] = val
-
-        obs_cache_len = len(obs_cache)
-        if obs_cache_len > 1000 and obs_cache_len % 50: # 1000 is an arbitrary threshold
-            logger.warn(f"Cache for obs {obs} is large: {obs_cache_len} items.")
-
-        # Save to pickle file
-        if save_to_file:
-            self.save_cache_file()
-
-    def load_obs_from_local_cache(self, paramvec: np.ndarray, obs: str):
-        if obs not in ['energy', 'energy_grad']:
-            raise ValueError(f"Unknown observable {obs} is not in cache.")
-        obs_cache = self.cache_data[obs]
-        for key in obs_cache.keys():
-            if np.allclose(self.key2paramvec(key), paramvec):
-                return obs_cache[key]
-        return None
-
-    def load_cache_file(self, cache_file: str):
-        # TODO: once we include other objects in the cache,
-        #       this function should check that cached objects have the same configs
-        if os.path.exists(cache_file):
-            with open(cache_file, "rb") as infile:
-                cache_data = pickle.load(infile)
-                if cache_data['cache_version'] == self.cache_version:
-                    self.cache_data = cache_data
-                    logger.info(f"Loaded cache file {cache_file}")
-                else:
-                    logger.warn(f"Cache file {cache_file} has version {cache_data['cache_version']} \
-                                but we are using version {self.cache_version}. Ignoring cached data.")
-                    # TODO: we can probably recover some of the data
-        return self.cache_data
 
 
 ####################### Minimizer #######################
@@ -277,14 +205,14 @@ class Minimizer():
 def print_callback(x, minimizer):
     
     res = minimizer.last_result
-    paramvec = minimizer.evaluator.system_cfg.paramvec
+    paramvec = minimizer.evaluator_manager.system_cfg.paramvec
 
     # If caching is on, the first time we call this function, the last_result will be None
     if res is None:
         logger.info("Callback message due to caching. The last_result is None.")
         return
     
-    if minimizer.evaluator.type == 'exact':
+    if minimizer.evaluator_manager.type == 'exact':
         acceptance_prob = np.nan # acceptance_prob is undefined for exact contraction
     else:
         acceptance_prob = res.get_obs_mean("acceptance_prob")
