74e80e60a7eb725ab3e41429fe502bc2d5ea5d36
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-01-16 14:07:06 +0200

    Make use of unified interface for ExactEvaluator and MonteCarloEvaluator in EvaluatorManager

--- ggpeps/evaluator_manager.py
+++ ggpeps/evaluator_manager.py
@@ -36,47 +36,43 @@ class EvaluatorManager:
             self.type = 'exact'
         else:
             self.type = 'mc'
-    
-    def simulate(self):
-        if self.type == 'exact':
-            # The exacteval implementation currently only supports a single runner
-            system = self.system_cls(self.system_cfg)
-            system.initialize()
-            exact_eval = ExactEvaluator(None, system)
-            exact_eval.evaluate()
-            return exact_eval
-        
-        elif self.type == 'mc':
-            """Start the simulation of the runners"""
-            resultvec = []
-            if self.nrunner > 0:
-                #system_cfg_id = ray.put(self.system_cfg)
-                reduced_meas_steps = self.mc_cfg.meas_steps // self.nrunner
-                logger.info(f"Starting {self.nrunner} runners with {reduced_meas_steps} measurement steps each (total: {self.nrunner * reduced_meas_steps}).")
-                for i in range(self.nrunner):
-                    # Make a copy of the MC config, and change the seed for each runner
-                    cfg = copy.deepcopy(self.mc_cfg)
-                    cfg.seed = self.mc_cfg.seed + i
-                    cfg.meas_steps = reduced_meas_steps
 
-                    # Make a copy of the system config 
-                    # This is necessary, because otherwise an error is raised when we try to modify the params
-                    # in enforce_parameter_conditions()
-                    # For some unclear reason, making a deep copy here does not prevent this, so instead a deep 
-                    # copy is made inside run_mc()
-                    #sys_cfg = copy.deepcopy(self.system_cfg)
-                    
-                    resultvec.append(run_mc.remote(i, cfg, self.system_cls, self.system_cfg))
-                resultvec = ray.get(resultvec)
-                return self.collect(resultvec)
-            else:
-                system = self.system_cls(self.system_cfg)
-                system.initialize()
-                mc = MonteCarloEvaluator(self.mc_cfg, system)
-                mc.evaluate()
-                return mc
-        
-        raise ValueError(f"Unknown evaluator type {self.type}")
+        system = self.system_cls(self.system_cfg)
+        system.initialize()
+        if self.type == 'exact':
+            self.evaluator = ExactEvaluator(None, system)
+        elif self.type == 'mc':
+            self.evaluator = MonteCarloEvaluator(self.mc_cfg, system)
+        else:
+            raise ValueError(f"Unknown evaluator type {self.type}")
+
+    def simulate(self):
+        if self.type == 'mc' and self.nrunner > 0: # The exacteval implementation currently only supports a single runner
+            """Start the simulation of the runners.
+            Currently only Monte Carlo is supported, and the runners cannot be resumed from where they left off."""
+            resultvec = []
+            #system_cfg_id = ray.put(self.system_cfg)
+            reduced_meas_steps = self.mc_cfg.meas_steps // self.nrunner
+            logger.info(f"Starting {self.nrunner} runners with {reduced_meas_steps} measurement steps each (total: {self.nrunner * reduced_meas_steps}).")
+            for i in range(self.nrunner):
+                # Make a copy of the MC config, and change the seed for each runner
+                cfg = copy.deepcopy(self.mc_cfg)
+                cfg.seed = self.mc_cfg.seed + i
+                cfg.meas_steps = reduced_meas_steps
+
+                # Make a copy of the system config 
+                # This is necessary, because otherwise an error is raised when we try to modify the params
+                # in enforce_parameter_conditions()
+                # For some unclear reason, making a deep copy here does not prevent this, so instead a deep 
+                # copy is made inside run_mc()
+                #sys_cfg = copy.deepcopy(self.system_cfg)
+                
+                resultvec.append(run_mc.remote(i, cfg, self.system_cls, self.system_cfg))
+            resultvec = ray.get(resultvec)
+            return self.collect(resultvec)
+        else:
+            self.evaluator.evaluate()
+            return self.evaluator
     
     def collect(self, resultvec):
         """Unify the results of multiple runners
