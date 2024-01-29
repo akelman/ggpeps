a4caba09c3920fb0483ad57a7322d6f42f149b76
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-01-29 19:07:55 +0200

    Add test for cache

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,7 +1,12 @@
 import unittest
 import numpy as np
 
-from ggpeps.minimizer import Cache
+import ggpeps.system as system
+import ggpeps.lattice as lattice
+from ggpeps.evaluator_manager import EvaluatorManager
+from ggpeps.minimizer import Minimizer, MinimizerConfig, Cache
+from ggpeps.mc import MonteCarloEvaluator, MonteCarloEvaluatorConfig
+
 
 class TestCache(unittest.TestCase):
 
@@ -13,3 +18,13 @@ class TestCache(unittest.TestCase):
         key = self.cache.paramvec2key(paramvec)
         restored_paramvec = self.cache.key2paramvec(key)
         np.testing.assert_allclose(paramvec, restored_paramvec)
+    
+    def test_cache_keys(self):
+        # Test that all cached variables are present
+        self.assertEqual(self.cache.cache_data['cache_version'], 0.1)
+        self.assertTrue('git_hash' in self.cache.cache_data.keys())
+
+        self.assertEqual(self.cache.cache_data['evaluator_manager'], None)
+        self.assertEqual(self.cache.cache_data['energy'], {})
+        self.assertEqual(self.cache.cache_data['energy_grad'], {})
+    
