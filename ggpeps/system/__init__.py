1e3db3d67d4ebf55f3e91c028c4c6c74d7bb32a6
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-02-04 14:21:33 +0200

    Remove dependency on TypeAlias, since it's not supported in python 3.9

--- ggpeps/system/__init__.py
+++ ggpeps/system/__init__.py
@@ -7,6 +7,6 @@ from ggpeps.system.system_z2_2d_G2c_F4c import Z2System2D_G2C_F4C_Config, Z2Syst
 from ggpeps.system.system_z2_2d_8c import Z2System2D_8C_Config, Z2System2D_8C
 
 # there may be a better way to this after python 3.12
-from typing import Union, TypeAlias
-SystemType: TypeAlias = Union[U1System2D, Z2System2D, Z2System2D2C, Z2System2D_G2C_F2C, Z2System2D_G2C_F4C, Z2System2D_8C]
-SystemConfigType: TypeAlias = Union[U1System2DConfig, Z2System2DConfig, Z2System2D2CConfig, Z2System2D_G2C_F2C_Config, Z2System2D_G2C_F4C_Config, Z2System2D_8C_Config]
+from typing import Union # TypeAlias
+SystemType = Union[U1System2D, Z2System2D, Z2System2D2C, Z2System2D_G2C_F2C, Z2System2D_G2C_F4C, Z2System2D_8C]
+SystemConfigType = Union[U1System2DConfig, Z2System2DConfig, Z2System2D2CConfig, Z2System2D_G2C_F2C_Config, Z2System2D_G2C_F4C_Config, Z2System2D_8C_Config]
