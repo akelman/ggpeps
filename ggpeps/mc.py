d8759b734c211850ea388d76105ebbeccb81b3e5
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-03-14 16:54:45 +0200

    Improve documentation with docstring and type hints

--- ggpeps/mc.py
+++ ggpeps/mc.py
@@ -82,7 +82,20 @@ class MonteCarloEvaluatorConfig:
 ################################### Multiprocessing layer #######################
     
 @ray.remote
-def run_mc(runner_id, mc_cfg: MonteCarloEvaluatorConfig, system_cls, system_cfg, logger_info:dict):
+def run_mc(runner_id: int, mc_cfg: MonteCarloEvaluatorConfig, system_cls, system_cfg, logger_info:dict):
+    """Worker for running part of a MC simulation.
+
+    Args:
+        runner_id (int): Runner ID
+        mc_cfg (MonteCarloEvaluatorConfig): 
+        system_cls (): 
+        system_cfg (): 
+        logger_info (dict): configs for the logger (logger needs to be set up in each worker)
+
+    Returns:
+        MonteCarloEvaluator
+    """
+
     # Setup logger
     logger = logging.getLogger('ggpeps')
     logger_file = logger_info['filename']
