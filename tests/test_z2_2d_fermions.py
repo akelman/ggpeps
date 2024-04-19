a528993ed8aa685a2077680667a7a1b990667810
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-04-18 20:25:25 -0400

    Add numerical gradient test for two-flavor mass gradients

--- tests/test_z2_2d_fermions.py
+++ tests/test_z2_2d_fermions.py
@@ -231,6 +231,49 @@ class TestZ2C4System(unittest.TestCase):
                     print(f"symbol: {symbolvec[ind]}, analytic: {deriv_ana[layerind,ind]}, numerical: {deriv_num}")
                     self.assertAlmostEqual(deriv_ana[layerind,ind], deriv_num, places=5)
 
+    def test_grad_mass_energy_2flavor(self):
+        # This is comparison of the analytic derivative against the numeric derivative
+        # for the 2 copy fermionic ansatz with 2 physical flavors
+        eps = 1e-5
+        paramvec = np.random.rand(2,20)
+        lat_2x2 = lattice.Lattice2D(2, 2)
+        system_cfg = system.Z2System2D_G2C_F2C_Config(lat_2x2, 0.0, 0.0, 0.0, 1.0, None, num_pg_layer=1, num_fermionic_layer=2)
+        system_cfg.paramvec = paramvec
+        system_z2_2_2 = system.Z2System2D_G2C_F2C(system_cfg)
+ 
+        config = np.array([0]*7 + [np.pi]*1)
+        system_z2_2_2.update_gauge_full_system(config)
+
+        deriv_ana = system_z2_2_2.mass_energy_op_grad_vec
+        symbolvec = system_z2_2_2.symbolvec
+        for layerind in range(3):
+            # we could skip the first layer, since the first layer does not contribute to the
+            # mass energy
+            for ind in range(len(symbolvec)):
+                with self.subTest(symbol=symbolvec[ind], layerind=layerind):
+                    paramvec_left = np.copy(paramvec)
+                    paramvec_right = np.copy(paramvec)
+                    paramvec_left[layerind, ind] -= eps
+                    paramvec_right[layerind, ind] += eps
+                    system_cfg_left = system.Z2System2D_G2C_F2C_Config(lat_2x2, 0.0, 0.0, 1.0, 1.0, None)
+                    system_cfg_right = system.Z2System2D_G2C_F2C_Config(lat_2x2, 0.0, 0.0, 1.0, 1.0, None)
+
+                    system_cfg_left.paramvec = paramvec_left
+                    system_cfg_right.paramvec = paramvec_right
+
+                    system_z2_2_2_left = system.Z2System2D_G2C_F2C(system_cfg_left)
+                    system_z2_2_2_right = system.Z2System2D_G2C_F2C(system_cfg_right)
+                    system_z2_2_2_left.update_gauge_full_system(config)
+                    system_z2_2_2_right.update_gauge_full_system(config)
+
+                    val_left = system_z2_2_2_left.mass_energy_op
+                    val_right = system_z2_2_2_right.mass_energy_op
+                    deriv_num = (val_right - val_left) / (2 * eps)
+
+                    print(f"left: {val_left}, right: {val_right}")
+                    print(f"symbol: {symbolvec[ind]}, analytic: {deriv_ana[layerind,ind]}, numerical: {deriv_num}")
+                    self.assertAlmostEqual(deriv_ana[layerind,ind], deriv_num, places=5)
+    
     @skip("This gradient tests with the 4 copy ansatz take to long")
     def test_grad_mass_energy_4C(self):
         # This is comparison of the analytic derivative against the numeric derivative
