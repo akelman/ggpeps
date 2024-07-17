a396e30f442df901a5f7e9fe9d2f51a1a44abf12
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-07-17 16:16:27 +0200

    Add broken tests

--- tests/test_backend_equivalence.py
+++ tests/test_backend_equivalence.py
@@ -7,10 +7,10 @@ from unittest import skip
 import numpy as np
 import jax.numpy as jnp
 
-from ggpeps import utils
+import ggpeps
 from ggpeps import lattice
-from ggpeps import system, exacteval
-from ggpeps.system.global_funcs import *
+#rom ggpeps import xnp
+#from ggpeps.system.global_funcs import *
 
 from tests import TEST_BACKEND
 
@@ -18,10 +18,13 @@ from tests import TEST_BACKEND
 
 class TestBackends(unittest.TestCase):
     def setUp(self):
+        # We don't create system objects, etc. here, 
+        # because we want to have explicit control over which backend is used
         pass
 
     def tearDown(self):
         # Reload ggpeps to ensure that the backend is set correctly for the rest of the tests
+        
         os.environ["GGPEPS_BACKEND"] = TEST_BACKEND
         importlib.reload(ggpeps)
     
@@ -44,5 +47,63 @@ class TestBackends(unittest.TestCase):
         np_slice = slice_matrix_numpy(np_mat, a, b, c, d)
         jax_slice = slice_matrix_jax(jax_mat, a, b, c, d)
         self.assertTrue(np.allclose(np_slice, np.array(jax_slice) ))
+
+    def test_tmat_equivalence(self):
+        print(dir(ggpeps))
+        lat = lattice.Lattice2D(2,2)
+        paramvec = np.random.rand(2, 20)
+
+        # numpy
+        os.environ["GGPEPS_BACKEND"] = "numpy"
+        importlib.reload(ggpeps)
+
+        cfg = ggpeps.system.Z2System2D_G2C_F2C_Config(lat, 1,1,1,1)
+        cfg.paramvec = paramvec
+        system_z2 = ggpeps.system.Z2System2D_G2C_F2C(cfg) 
+        system_z2.cfg.enforce_parameter_conditions(system_z2.cfg.paramvec)
+        tmat_vec_np = system_z2.tmat_vec
+
+        # jax
+        os.environ["GGPEPS_BACKEND"] = "jax"
+        importlib.reload(ggpeps)
+
+        cfg = ggpeps.system.Z2System2D_G2C_F2C_Config(lat, 1,1,1,1)
+        cfg.paramvec = paramvec
+        system_z2 = ggpeps.system.Z2System2D_G2C_F2C(cfg) 
+        system_z2.cfg.enforce_parameter_conditions(system_z2.cfg.paramvec)
+        tmat_vec_jax = system_z2.tmat_vec
+
+        self.assertTrue(np.allclose(tmat_vec_np, np.array(tmat_vec_jax)))
+
+    def test_gamma_dirac_vec(self):
+        #global ggpeps
+        #import ggpeps
+        lat = ggpeps.lattice.Lattice2D(2,2)
+        paramvec = np.random.rand(2, 20)
+        
+        # jax
+        #def test_jax():
+        os.environ["GGPEPS_BACKEND"] = "jax"
+        #importlib.invalidate_caches()
+        importlib.reload(ggpeps)
+
+        cfg = ggpeps.system.Z2System2D_G2C_F2C_Config(lat, 1,1,1,1)
+        cfg.paramvec = paramvec
+        system_z2 = ggpeps.system.Z2System2D_G2C_F2C(cfg) 
+        system_z2.cfg.enforce_parameter_conditions(system_z2.cfg.paramvec)
+        gamma_dirac_vec_jax = system_z2.gamma_dirac_vec
+            #return gamma_dirac_vec_jax
+
+        # numpy
+        os.environ["GGPEPS_BACKEND"] = "numpy"
+        importlib.reload(ggpeps)
+
+        cfg = ggpeps.system.Z2System2D_G2C_F2C_Config(lat, 1,1,1,1)
+        cfg.paramvec = paramvec
+        system_z2 = ggpeps.system.Z2System2D_G2C_F2C(cfg) 
+        system_z2.cfg.enforce_parameter_conditions(system_z2.cfg.paramvec)
+        gamma_dirac_vec_np = system_z2.gamma_dirac_vec
+        print(type(gamma_dirac_vec_jax))
+        self.assertTrue(np.allclose(gamma_dirac_vec_np, np.array(gamma_dirac_vec_jax)))
 
 
