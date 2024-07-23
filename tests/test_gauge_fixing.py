cd53e70b7bb4166b49d9f2c103734766b5a9a83a
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-07-23 11:11:33 +0300

    Clean up gauge fixing tests somewhat Increase number of MC steps in test of MC with gauge fixing

--- tests/test_gauge_fixing.py
+++ tests/test_gauge_fixing.py
@@ -22,8 +22,6 @@ class Testgaugefixing(unittest.TestCase):
     def setUp(self):
         self.lat2 = lattice.Lattice2D(2,2)
         self.lat4 = lattice.Lattice2D(4,4)
-        self.tree2 = self.lat2.generate_maximal_tree()
-        self.tree4 = self.lat4.generate_maximal_tree()
         
         paramvec = np.random.rand(2, 20)
         
@@ -34,7 +32,7 @@ class Testgaugefixing(unittest.TestCase):
         eval_cfg = None
         self.evaluator2 = exacteval.ExactEvaluator(eval_cfg, self.system_z2_2, False)
         self.configvec2 = self.evaluator2.generate_config_vec()
-        self.netural_gauge2 = self.system_z2_2.gaugemgr.get_neutral_gauge_value()        
+        self.neutral_gauge2 = self.system_z2_2.gaugemgr.get_neutral_gauge_value()        
 
         cfg4 = system.Z2System2D_G2C_F2C_Config(self.lat4, 1,1,1,1)
         self.system_z2_4 = system.Z2System2D_G2C_F2C(cfg4) 
@@ -44,8 +42,8 @@ class Testgaugefixing(unittest.TestCase):
         self.configvec4 = self.evaluator4.generate_config_vec()
         self.netural_gauge4 = self.system_z2_4.gaugemgr.get_neutral_gauge_value() 
 
-        warmup_steps = 2000
-        meas_steps = 1000
+        warmup_steps = 10000 #20000
+        meas_steps = 15000 #40000
         binsize = 1
         update_size_per_step = 2
 
@@ -55,29 +53,31 @@ class Testgaugefixing(unittest.TestCase):
         mc_config.meas_steps = meas_steps
         mc_config.binsize = binsize
         mc_config.update_size_per_step = update_size_per_step
-        self.mc_evaluator = MonteCarloEvaluator(mc_config, self.system_z2_2, False)
-        self.mc_evaluator.gauge_fixing = False
 
-        #define MC evaluator without gauge fixing
-        mc_config_gf = MonteCarloEvaluatorConfig()
-        mc_config_gf.warmup_steps = warmup_steps
-        mc_config_gf.meas_steps = meas_steps
-        mc_config_gf.binsize = binsize
-        mc_config_gf.update_size_per_step = update_size_per_step
-        self.mc_evaluator_gf = MonteCarloEvaluator(mc_config_gf, self.system_z2_2, False)
-        self.mc_evaluator_gf.gauge_fixing = True
+
+        paramvec = np.random.rand(2, 20)
+        
+        cfg = system.Z2System2D_G2C_F2C_Config(self.lat2, 1,1,1,1)
+        cfg.paramvec = paramvec
+        system_z2_2_A = system.Z2System2D_G2C_F2C(cfg) 
+        system_z2_2_B = system.Z2System2D_G2C_F2C(cfg) 
+
+        self.mc_evaluator = MonteCarloEvaluator(mc_config, system_z2_2_A, gauge_fixing=False)
+        self.mc_evaluator_gf = MonteCarloEvaluator(mc_config, system_z2_2_B, gauge_fixing=True)
 
 
 
     def test_configvec(self):
         """Ensure that the configvec for gauge fixing is generated correctly. Ensure that the links in the tree are set to the unity in all configurations 
         and that all configurations are unique."""
+
         self.assertEqual(len(self.configvec4), 2**self.lat4.ncomptreelinks) 
+
         tuple_configvec2 = [] # converting each configuration in configvec to a tuple - because it's hashable
-        for config in self.configvec2: #2x2 lattice
+        for config in self.configvec2: # 2x2 lattice
             tuple_configvec2.append(tuple(config)) 
-            for link in self.tree2:
-                self.assertEqual(config[link], self.netural_gauge2)
+            for link in self.lat2.maximal_tree:
+                self.assertEqual(config[link], self.neutral_gauge2)
 
         unique_configvec2 = set(tuple_configvec2) # configvec with unique combinations only
         self.assertEqual(len(tuple_configvec2),len(unique_configvec2)) # assert that there are no repeated configurations
@@ -86,7 +86,7 @@ class Testgaugefixing(unittest.TestCase):
         tuple_configvec4 = [] 
         for config in self.configvec4: #4x4 lattice
             tuple_configvec4.append(tuple(config))
-            for link in self.tree4:
+            for link in self.lat4.maximal_tree:
                 self.assertEqual(config[link],self.netural_gauge4)
         unique_configvec4 = set(tuple_configvec4) # configvec with unique combinations only
         self.assertEqual(len(tuple_configvec4),len(unique_configvec4))
@@ -127,16 +127,17 @@ class Testgaugefixing(unittest.TestCase):
 
 #    @skip("Too long")
     def test_mceval(self):
+
         start = time.time()
         self.mc_evaluator.evaluate()
         no_gauge_fixing_energy = self.mc_evaluator.get_obs_mean("energy")
-        end= time.time()
-        print(no_gauge_fixing_energy,"no gf",end-start)
+        end = time.time()
+        print(f"Energy (no gf): {no_gauge_fixing_energy}, time: {end-start}")
         start = time.time()
         self.mc_evaluator_gf.evaluate()
         gauge_fixing_energy = self.mc_evaluator_gf.get_obs_mean("energy")
         end = time.time()
-        print(gauge_fixing_energy,"gf",end-start)
+        print(f"Energy (gf): {gauge_fixing_energy}, time: {end-start}")
         self.assertAlmostEqual(gauge_fixing_energy,no_gauge_fixing_energy,places=0)
 
         # for k,val in no_gauge_fixing_eval.items():
