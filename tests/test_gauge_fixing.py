a2ac11a929ac78f6d4aac397ce1b7b2c4a00ce1f
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-07-23 13:17:51 +0300

    Clean up gauge fixing tests All tests pass

--- tests/test_gauge_fixing.py
+++ tests/test_gauge_fixing.py
@@ -1,19 +1,13 @@
 import unittest 
 from unittest import skip
-import time
-
 
 import numpy as np
 
-from ggpeps import utils
 from ggpeps import lattice
 from ggpeps import system, exacteval
 
-from ggpeps.lattice import Direction
 from ggpeps.mc import MonteCarloEvaluatorConfig, MonteCarloEvaluator
-from ggpeps.system import Z2System2D_G2C_F2C_Config, Z2System2D_G2C_F2C
-from ggpeps.minimizer import Minimizer, MinimizerConfig
-from ggpeps.utils import compare_array_elementwise
+
 
 # ======================= Z2 fermionic system (4 copies) =========================================
 
@@ -23,17 +17,19 @@ class Testgaugefixing(unittest.TestCase):
         self.lat2 = lattice.Lattice2D(2,2)
         self.lat4 = lattice.Lattice2D(4,4)
         
+        eval_cfg = None
         paramvec = np.random.rand(2, 20)
         
+        # Build 2x2 system and evaluator
         cfg2 = system.Z2System2D_G2C_F2C_Config(self.lat2, 1,1,1,1)
         cfg2.paramvec = paramvec
         self.system_z2_2 = system.Z2System2D_G2C_F2C(cfg2) 
         self.system_z2_2.cfg.enforce_parameter_conditions(self.system_z2_2.cfg.paramvec)
-        eval_cfg = None
         self.evaluator2 = exacteval.ExactEvaluator(eval_cfg, self.system_z2_2, False)
         self.configvec2 = self.evaluator2.generate_config_vec()
         self.neutral_gauge2 = self.system_z2_2.gaugemgr.get_neutral_gauge_value()        
 
+        # Build 4x4 system and evaluator
         cfg4 = system.Z2System2D_G2C_F2C_Config(self.lat4, 1,1,1,1)
         self.system_z2_4 = system.Z2System2D_G2C_F2C(cfg4) 
         cfg4.paramvec = paramvec
@@ -42,36 +38,13 @@ class Testgaugefixing(unittest.TestCase):
         self.configvec4 = self.evaluator4.generate_config_vec()
         self.netural_gauge4 = self.system_z2_4.gaugemgr.get_neutral_gauge_value() 
 
-        warmup_steps = 10000 #20000
-        meas_steps = 15000 #40000
-        binsize = 1
-        update_size_per_step = 2
+    def test_configvec_2x2(self):
+        """Ensure that the configvec for gauge fixing is generated correctly. 
+        Ensure that the links in the tree are set to the unity in all configurations 
+        and that all configurations are unique.
+        """
 
-        #define MC evaluator without gauge fixing
-        mc_config = MonteCarloEvaluatorConfig()
-        mc_config.warmup_steps = warmup_steps
-        mc_config.meas_steps = meas_steps
-        mc_config.binsize = binsize
-        mc_config.update_size_per_step = update_size_per_step
-
-
-        paramvec = np.random.rand(2, 20)
-        
-        cfg = system.Z2System2D_G2C_F2C_Config(self.lat2, 1,1,1,1)
-        cfg.paramvec = paramvec
-        system_z2_2_A = system.Z2System2D_G2C_F2C(cfg) 
-        system_z2_2_B = system.Z2System2D_G2C_F2C(cfg) 
-
-        self.mc_evaluator = MonteCarloEvaluator(mc_config, system_z2_2_A, gauge_fixing=False)
-        self.mc_evaluator_gf = MonteCarloEvaluator(mc_config, system_z2_2_B, gauge_fixing=True)
-
-
-
-    def test_configvec(self):
-        """Ensure that the configvec for gauge fixing is generated correctly. Ensure that the links in the tree are set to the unity in all configurations 
-        and that all configurations are unique."""
-
-        self.assertEqual(len(self.configvec4), 2**self.lat4.ncomptreelinks) 
+        self.assertEqual(len(self.configvec2), 2**self.lat2.ncomptreelinks) 
 
         tuple_configvec2 = [] # converting each configuration in configvec to a tuple - because it's hashable
         for config in self.configvec2: # 2x2 lattice
@@ -80,65 +53,62 @@ class Testgaugefixing(unittest.TestCase):
                 self.assertEqual(config[link], self.neutral_gauge2)
 
         unique_configvec2 = set(tuple_configvec2) # configvec with unique combinations only
-        self.assertEqual(len(tuple_configvec2),len(unique_configvec2)) # assert that there are no repeated configurations
-        
-        # now for 4x4 lattice
+        self.assertEqual(len(tuple_configvec2), len(unique_configvec2)) # assert that there are no repeated configurations
+    
+    def test_configvec_4x4(self):
+        """Test configvec for 4x4 lattice"""
+
+        self.assertEqual(len(self.configvec4), 2**self.lat4.ncomptreelinks) 
+
         tuple_configvec4 = [] 
         for config in self.configvec4: #4x4 lattice
             tuple_configvec4.append(tuple(config))
             for link in self.lat4.maximal_tree:
-                self.assertEqual(config[link],self.netural_gauge4)
+                self.assertEqual(config[link], self.netural_gauge4)
+        
         unique_configvec4 = set(tuple_configvec4) # configvec with unique combinations only
-        self.assertEqual(len(tuple_configvec4),len(unique_configvec4))
+        self.assertEqual(len(tuple_configvec4), len(unique_configvec4))
     
     def test_exacteval(self):
         """Ensure that exact evaluation gives the same results with and without gauge fixing"""
+
         self.evaluator2.gauge_fixing = False
-        start_time = time.time()
         no_gauge_fixing_eval = self.evaluator2.evaluate()
-        end_time = time.time()
-        print("no gauge_fixing",end_time-start_time)
 
         self.evaluator2.obsdict = None
         self.evaluator2.gauge_fixing = True
-        start_time = time.time()
         gauge_fixing_eval = self.evaluator2.evaluate()
-        end_time = time.time()
-        print("gauge_fixing",end_time-start_time)
-        for k,val in no_gauge_fixing_eval.items():
-            self.assertTrue(np.allclose(val, gauge_fixing_eval[k]))
-    
-    # def test_exacteval4(self):
-    # """Ensure that exact evaluation gives the same results with and without gauge fixing, for 2x2. Running time too lomg for personal computer. """ 
-    #     self.evaluator4.gauge_fixing = True
-    #     start_time = time.time()
-    #     no_gauge_fixing_eval = self.evaluator4.evaluate()
-    #     end_time = time.time()
-    #     print("gauge_fixing",end_time-start_time)
 
-    #     self.evaluator4.obsdict = None
-    #     self.evaluator4.gauge_fixing = False
-    #     start_time = time.time()
-    #     gauge_fixing_eval = self.evaluator4.evaluate()
-    #     end_time = time.time()
-    #     print("no gauge_fixing",end_time-start_time)
-    #     for k,val in no_gauge_fixing_eval.items():
-    #         self.assertTrue(np.allclose(val, gauge_fixing_eval[k]))
+        for key, val in no_gauge_fixing_eval.items():
+            self.assertTrue(np.allclose(val, gauge_fixing_eval[key]))
 
-#    @skip("Too long")
+    # @skip("Too long")
     def test_mceval(self):
 
-        start = time.time()
-        self.mc_evaluator.evaluate()
-        no_gauge_fixing_energy = self.mc_evaluator.get_obs_mean("energy")
-        end = time.time()
-        print(f"Energy (no gf): {no_gauge_fixing_energy}, time: {end-start}")
-        start = time.time()
-        self.mc_evaluator_gf.evaluate()
-        gauge_fixing_energy = self.mc_evaluator_gf.get_obs_mean("energy")
-        end = time.time()
-        print(f"Energy (gf): {gauge_fixing_energy}, time: {end-start}")
+        # MC config
+        mc_config = MonteCarloEvaluatorConfig()
+        mc_config.warmup_steps = 20000 #20000
+        mc_config.meas_steps = 20000 #40000
+        mc_config.binsize = 1
+        mc_config.update_size_per_step = 2
+        
+        # MC evaluators - with and without gauge fixing
+        cfg = system.Z2System2D_G2C_F2C_Config(self.lat2, 1,1,1,1)
+        cfg.paramvec = np.random.rand(2, 20)
+        system_z2_2_A = system.Z2System2D_G2C_F2C(cfg) 
+        system_z2_2_B = system.Z2System2D_G2C_F2C(cfg) 
+
+        mc_evaluator_no_gf = MonteCarloEvaluator(mc_config, system_z2_2_A, gauge_fixing=False)
+        mc_evaluator_gf = MonteCarloEvaluator(mc_config, system_z2_2_B, gauge_fixing=True)
+
+        # Evaluations
+        res_no_gf = mc_evaluator_no_gf.evaluate()
+        no_gauge_fixing_energy = mc_evaluator_no_gf.get_obs_mean("energy")
+
+        res_gf = mc_evaluator_gf.evaluate()
+        gauge_fixing_energy = mc_evaluator_gf.get_obs_mean("energy")
+
         self.assertAlmostEqual(gauge_fixing_energy,no_gauge_fixing_energy,places=0)
 
-        # for k,val in no_gauge_fixing_eval.items():
-        #     self.assertTrue(np.allclose(val, gauge_fixing_eval[k]))
+        #for key, val in res_no_gf.items():
+        #    self.assertTrue(np.allclose(val, res_gf[key]))
