0c6dd3a073a32dba00d1ee6394904e4762739ba0
Patrick Emonts <emonts@lorentz.leidenuniv.nl>
2024-08-06 11:45:01 +0200

    create_csv: formatting

--- create_csv.py
+++ create_csv.py
@@ -4,22 +4,24 @@ import pandas as pd
 
 def main(args):
 
-    #cols = ['el', 'mag', 'int', 'mass', 'energy', 'el_energy', 'mag_energy', 'mass_energy', 'int_energy', 'mass_energy', 'tag']
+    # cols = ['el', 'mag', 'int', 'mass', 'energy', 'el_energy', 'mag_energy', 'mass_energy', 'int_energy', 'mass_energy', 'tag']
     data = pd.DataFrame()
-    obs = ['energy', 'el_energy', 'mag_energy', 'int_energy', 'mass_energy', 'norm']
+    obs = ['energy', 'el_energy', 'mag_energy',
+           'int_energy', 'mass_energy', 'norm']
 
     for fname in args.files:
         if os.path.isfile(fname):
             df = pd.read_pickle(fname)
             vals = df.loc[:, 'mean']
-            try: 
+            try:
                 error = df.loc[:, 'err']
             except:
                 error = vals
             keys = df.loc[:, 'name']
             tag = args.tag
 
-            d = {'el': df.loc[0, 'g_el'], 'mag': df.loc[0, 'g_mag'], 'int': df.loc[0, 'g_int'], 'mass': df.loc[0, 'g_mass'], 'tag': tag}
+            d = {'el': df.loc[0, 'g_el'], 'mag': df.loc[0, 'g_mag'],
+                 'int': df.loc[0, 'g_int'], 'mass': df.loc[0, 'g_mass'], 'tag': tag}
             for key, val, error in zip(keys, vals, error):
                 if key in obs:
                     d[key] = val
@@ -29,10 +31,10 @@ def main(args):
             for key, val in d.items():
                 d[key] = [val]
 
-            #data = data.append(d, ignore_index=True) # deprecated
+            # data = data.append(d, ignore_index=True) # deprecated
             file_data = pd.DataFrame.from_dict(d, orient='columns')
             data = pd.concat([data, file_data])
-            
+
     data.to_csv(args.out)
     print(f"Data: {data.shape}")
 
@@ -45,7 +47,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--files", nargs="+", help="Directory")
     parser.add_argument("--out", help="Output CSV file")
-    parser.add_argument("--tag", type=str, default='', help="Tag to add to each row of the CSV file")
+    parser.add_argument("--tag", type=str, default='',
+                        help="Tag to add to each row of the CSV file")
 
     args = parser.parse_args()
 
