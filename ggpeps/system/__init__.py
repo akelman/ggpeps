cd2b3bad0be009ed03bee9a5cb5ffe478cd0e71d
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-08-14 19:51:19 +0300

    Fix imports after filename change in previous commit

--- ggpeps/system/__init__.py
+++ ggpeps/system/__init__.py
@@ -1,6 +1,6 @@
 __all__=["system_u1_2d", "system_z2_2d", "system_z2_2d_2c", "system_z2_2d_G2c_F2c", "system_z2_2d_G2c_F4c"]
 from ggpeps.system.system_u1_2d import U1System2DConfig, U1System2D
-from ggpeps.system.system_z2_2d import Z2System2DConfig, Z2System2D_1c
+from ggpeps.system.system_z2_2d_1c import Z2System2DConfig, Z2System2D_1c
 from ggpeps.system.system_z2_2d_2c import Z2System2D2CConfig, Z2System2D2C
 from ggpeps.system.system_z2_2d_G2c_F2c import Z2System2D_G2C_F2C_Config, Z2System2D_G2C_F2C
 from ggpeps.system.system_z2_2d_G2c_F4c import Z2System2D_G2C_F4C_Config, Z2System2D_G2C_F4C
