d1e85782284b351ad795415c552267f5bb43d979
Itay <itay.gomelski@mail.huji.ac.il>
2024-10-14 15:54:55 +0300

    A file that generates plots and saves plot binning analysis and and plot_timeseries

---
+++ plot/plots_MC.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+
+class Args:
+    def __init__(self, obs, fname):
+        self.obs = obs
+        self.fnames = fname
+        self.obs = obs
+
+
+def save_plot(func, fname, obs, name):
+    args = Args(obs, fname)
+    func(args, save_path=name)
+
+
+if __name__ == "__main__":
+    import glob
+    import plot_binning_analysis
+    import plot_timeseries
+
+    # base_path = "G:/My Drive/Research/MC/test_gauge_fixing_and_system_size/g_2.5_el_1.2500_mag_0.2000_int_1.0_mass_1.0/"  # g =2.5
+    # base_path = "G:/My Drive/Research/MC/test_gauge_fixing_and_system_size/g_0.5_el_0.2500_mag_1.0000_int_1.0_mass_1.0/"  # g = 0.5
+    base_path = "G:/My Drive/Research/MC/test_gauge_fixing_and_system_size/g_1.0_el_0.5000_mag_0.5000_int_1.0_mass_1.0/"  # g = 1.0
+    pattern_L6 = base_path + "L_6/L_*/**/*.gz"
+    pattern_L4 = base_path + "L_4/L_*/**/*.gz"
+    pattern_L2 = base_path + "L_2/L_*/**/*.gz"
+
+    file_list_L6 = glob.glob(pattern_L6, recursive=True)  # List of all .gz files in L_6
+    file_list_L4 = glob.glob(pattern_L4, recursive=True)  # List of all .gz files in L_4
+    file_list_L2 = glob.glob(pattern_L2, recursive=True)  # List of all .gz files in L_2
+
+    fname_lst = [file_list_L6, file_list_L4, file_list_L2]
+    obs_lst = ["energy", "mag_energy", "el_energy", "mass_energy", "int_energy"]
+    for fname in fname_lst:
+        for obs in obs_lst:
+            save_plot(
+                plot_timeseries.main,
+                fname,
+                obs,
+                fname[0][0 : len(base_path) + 4] + obs + "_timeseries.png",
+            )
