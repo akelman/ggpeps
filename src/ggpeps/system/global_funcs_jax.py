a0bf665925d01dec5eb647e7ce62691e0aee968f
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2024-10-15 15:21:47 +0300

    Revert "Add jit for jax functions" The jit caused issues with jax array slicing that need to be sorted out first This reverts commit d918fe90353a2baa10103c4a96175e5d8425d8d1.

--- src/ggpeps/system/global_funcs_jax.py
+++ src/ggpeps/system/global_funcs_jax.py
@@ -13,7 +13,6 @@ jax.config.update("jax_enable_x64", True)
 import ggpeps
 
 
-@jit
 def calculate_lognormvec_jax(
     gamma_in_sys_vec,
     mat_d_vec,
@@ -42,7 +41,6 @@ def calculate_lognormvec_jax(
     return dest / 2
 
 
-@jit
 def compute_grad_over_norm_jax(
     gamma_in_sys: jnp.ndarray,
     diff: jnp.ndarray,
@@ -83,7 +81,6 @@ def compute_grad_over_norm_jax(
     return dest
 
 
-@jit
 def compute_el_grad_vec_jax(system):
     """Computation of the electric energy gradients.
     We start by calculating the electric energies, since these are needed for evaluating the gradients.
@@ -196,7 +193,6 @@ def compute_el_grad_vec_jax(system):
     return dest_grad
 
 
-@jit
 def extract_partial_covmats_jax(mat, corner):
     """Extract the partial covariance matrices from a gaussian mapping
 
