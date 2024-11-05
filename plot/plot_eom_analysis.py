8fd511c8db9fd8e6e446dcf35dff7776899f5bda
Itay <itay.gomelski@mail.huji.ac.il>
2024-11-05 14:15:08 +0200

    Create a python file for plotting eom for update_size analysis taking into account autocorrelation decay time.

---
+++ plot/plot_eom_analysis.py
@@ -0,0 +1,54 @@
+import os
+import re
+import sys
+from ggpeps import utils
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import gzip
+
+
+def main(args, save_path=None):
+    f, axvec = plt.subplots(4, 1, figsize=(20, 13))
+    for fname in args.fname:
+        if os.path.isfile(fname):
+            basename, ext = os.path.splitext(fname)
+            if ext == ".gz":
+                with gzip.open(fname, "rb") as infile:
+                    dumpobj = pickle.load(infile)
+                    obsvec = np.asarray(
+                        dumpobj["mc"].obsdict[args.obs].get_timeseries()
+                    )
+            elif ext == ".txt":
+                obsvec = np.genfromtxt(fname)
+            else:
+                print(f"Unkown file type {ext}. Aborting.", file=sys.stderr)
+                sys.exit(1)
+            rangevals, meanarr, eomarr, stdarr = utils.rebin_error(obsvec)
+            axvec[0].plot(rangevals, meanarr, "o", label=fname)
+            axvec[1].plot(rangevals, stdarr, "o")
+            axvec[2].plot(rangevals, eomarr, "o")
+            axvec[3].plot(np.abs(utils.autocorr_fft(obsvec))[0:200], "o")
+        else:
+            print(f"File '{fname}' not found.", file=sys.stderr)
+
+    axvec[1].set_yscale("log")
+    axvec[0].set_xscale("log")
+    axvec[1].set_xscale("log")
+    axvec[2].set_xscale("log")
+    axvec[0].set_ylabel(f"Mean {args.obs}")
+    axvec[0].legend()
+    axvec[1].set_ylabel(f"STD {args.obs}")
+    axvec[2].set_ylabel(f"EOM {args.obs}")
+    axvec[3].set_ylabel(f"Autocorrelation {args.obs}")
+    axvec[2].set_xlabel("len(bin)")
+    axvec[3].set_xlabel(r"$\tau$")
+    axvec[3].set_yscale("log")
+    # f.tight_layout()
+    if save_path:
+        plt.savefig(save_path, dpi=300, bbox_inches="tight")
+        plt.close()
+    else:
+        plt.show()
