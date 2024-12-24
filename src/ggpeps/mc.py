b9009cb6e67d54f0360260bbf71294664e57cd9c
Itay <itay.gomelski@mail.huji.ac.il>
2024-12-24 16:06:45 +0200

    Add to the log file in debug level the acceptance probability.

--- src/ggpeps/mc.py
+++ src/ggpeps/mc.py
@@ -311,7 +311,12 @@ class MonteCarloEvaluator(Evaluator):
         logger.debug("Starting MC measurement")
         while self.step < self.cfg.warmup_steps + self.cfg.meas_steps:
             if self.step % self.cfg.run_log_freq == 0:
-                logger.debug(f"Run: {self.step}")
+                acceptance_ratio = self.obsdict["acceptance_prob"][
+                    -self.cfg.run_log_freq : :
+                ]
+                logger.debug(
+                    f"Run: {self.step}. Acceptance ratio of last {self.cfg.run_log_freq} steps is {acceptance_ratio}"
+                )
             self.update()
             self.measure()
             self.step += 1
