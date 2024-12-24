ebfdc6c760aa6913948c313cd2c7307b228eb26e
Itay <itay.gomelski@mail.huji.ac.il>
2024-12-24 16:46:21 +0200

    debug another small thing

--- src/ggpeps/mc.py
+++ src/ggpeps/mc.py
@@ -311,9 +311,9 @@ class MonteCarloEvaluator(Evaluator):
         logger.debug("Starting MC measurement")
         while self.step < self.cfg.warmup_steps + self.cfg.meas_steps:
             if self.step % self.cfg.run_log_freq == 0:
-                acceptance_ratio = self.obsdict["acceptance_prob"].datavec[
-                    -self.cfg.run_log_freq : :
-                ]
+                acceptance_ratio = np.mean(
+                    self.obsdict["acceptance_prob"].datavec[-self.cfg.run_log_freq : :]
+                )
                 logger.debug(
                     f"Run: {self.step}. Acceptance ratio of last {self.cfg.run_log_freq} steps is {acceptance_ratio}"
                 )
