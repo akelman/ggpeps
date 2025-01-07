5ee211b1535c765b31c147915b2fec5103393df7
Itay <itay.gomelski@mail.huji.ac.il>
2025-01-07 10:01:42 +0200

    Add an option of plottinggradients observables

--- plot/plot_eom_analysis.py
+++ plot/plot_eom_analysis.py
@@ -54,7 +54,13 @@ def main(args, save_path=None):
             else:
                 print(f"Unkown file type {log_ext}. Aborting.", file=sys.stderr)
                 sys.exit(1)
-
+            if (
+                "grad" in args.obs
+                and args.grad_ind is not None
+                and args.layer_num is not None
+            ):
+                # if it is a gradient, we plot the graph just for the index.
+                obsvec = obsvec[:, args.layer_num, args.grad_ind]
             dyn_mean, dyn_eom = compute_dynamic_eom_mean(obsvec, step_numbers)
 
             axvec[0].plot(step_numbers, dyn_mean, "o", label=args.pkl_fname[i])
@@ -71,7 +77,11 @@ def main(args, save_path=None):
     axvec[0].set_xlabel("step number")
     axvec[1].set_ylabel(f"Dynamical EOM {args.obs}")
     axvec[1].set_xlabel(f"time[sec]")
+    axvec[1].set_yscale("log")
+    axvec[1].set_xscale("log")
     axvec[2].set_ylabel(f"Dynamical EOM {args.obs}")
+    axvec[2].set_yscale("log")
+    axvec[2].set_xscale("log")
     axvec[2].set_xlabel(f"step number")
 
     # f.tight_layout()
@@ -108,34 +118,64 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--pkl_fname", nargs="+", help="MC pickle file")
     parser.add_argument("--obs", type=str, default="energy", help="Observable")
+    parser.add_argument(
+        "--grad_ind", type=int, default=None, help="Gradient index (default: 0)"
+    )
+    parser.add_argument(
+        "--layer_num",
+        type=int,
+        default=None,
+        help="Layer number - when calculating gradient",
+    )
     parser.add_argument("--log_fname", nargs="+", help="MC log file - on debug mode")
 
     # args = parser.parse_args()
 
     class Args:
-        def __init__(self, obs, pkl_fname, log_fname):
+        def __init__(self, obs, pkl_fname, log_fname, grad_ind=None, layer_num=None):
             self.obs = obs
             self.pkl_fname = pkl_fname
             self.log_fname = log_fname
             self.obs = obs
+            self.grad_ind = grad_ind
+            self.layer_num = layer_num
 
-    base_dir = "G:/My Drive/Research/MC/test_step_size/g_2.5_el_1.2500_mag_0.2000_int_1.0_mass_1.0/L4"
-
-    # Lists to store file paths
-    gz_files = []
-    log_files = []
-
+    base_dir_single = "G:/My Drive/Research/MC/trans_inv_mag/single_plaquette"
+    base_dir_all = "G:/My Drive/Research/MC/trans_inv_mag/all_plaquettes"
+    obs_lst = [
+        "energy",
+        "mag_energy",
+        "el_energy",
+        "mass_energy",
+        "int_energy",
+    ]
+    # obs_lst = ["energy_grad"]
     # Iterate over the folders named 'L_4_gf_F_update_size_1' to 'L_4_gf_F_update_size_15'
-    for i in range(1, 15, 2):
-        folder_name = f"L_4_gf_F_update_size_{i}"
-        folder_path = os.path.join(base_dir, folder_name)
+    for L in range(2, 7, 2):
+        for g in [0.1, 0.5, 1.0, 2.5]:
+            gz_files = []
+            log_files = []
+            folder_name = f"L_{L}_g_{g}"
+            folder_path_single = os.path.join(base_dir_single, folder_name)
+            folder_path_all = os.path.join(base_dir_all, folder_name)
 
-        # Find .gz and .log files in each folder
-        gz_file = glob.glob(os.path.join(folder_path, "*.gz"))
-        log_file = glob.glob(os.path.join(folder_path, "*.log"))
-        # Ensure we found exactly one of each file
-        if len(gz_file) == 1 and len(log_file) == 1:
-            gz_files.append(gz_file[0])
-            log_files.append(log_file[0])
-    args = Args("energy", gz_files, log_files)
-    main(args)
+            # Find .gz and .log files in each folder
+            gz_files.append(glob.glob(os.path.join(folder_path_single, "*.gz"))[0])
+            log_files.append(glob.glob(os.path.join(folder_path_single, "*.log"))[0])
+            gz_files.append(glob.glob(os.path.join(folder_path_all, "*.gz"))[0])
+            log_files.append(glob.glob(os.path.join(folder_path_all, "*.log"))[0])
+            for obs in obs_lst:
+                args = Args(obs, gz_files, log_files)
+                # for layer_num in range(2):
+                # args.layer_num = layer_num
+                # for grad_ind in range(20):
+                # args.grad_ind = grad_ind
+                main(
+                    args,
+                    gz_files[1][0 : len(base_dir_single) + 9] + "\\" + obs
+                    # + "_layer_num_"
+                    # + str(args.layer_num)
+                    # + "_grad_ind_"
+                    # + str(args.grad_ind)
+                    + "_log_log_scale" + "_eom_analysis.pdf",
+                )
