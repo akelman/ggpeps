3a3d260e0726ed4dd81bbd8d5b22f1480ae77338
Itay <itay.gomelski@mail.huji.ac.il>
2025-02-11 10:29:45 +0200

    Add a small clearification in the logger for gauge_fixing

--- manager.py
+++ manager.py
@@ -395,7 +395,7 @@ def main(args):
     elif args.gauge_fixing == 0:
         logger.info(f"Gauge fixing: False")
     else:
-        logger.info(f"Gauge fixing: {args.gauge_fixing}")
+        logger.info(f"Gauge fixing: {args.gauge_fixing} rows fixed")
     logger.info(f"g (lambda): {g}")
     logger.info(f"g_el: {g_el}")
     logger.info(f"g_mag: {g_mag}")
