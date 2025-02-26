6ff7e7efb4e11f9873b5dbe7907124cc9ea81bf6
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2025-02-26 15:43:25 +0200

    Fix handling of extracting chem couplings from foldername

--- src/ggpeps/utils.py
+++ src/ggpeps/utils.py
@@ -860,13 +860,19 @@ def show_eigenvalues(mat):
 
 
 def get_couplings_from_foldername(fname):
-    couplings = ["g", "el", "mag", "int", "mass", "chem"]
+    couplings = ["g", "el", "mag", "int", "mass"]
     res = ""
     for arg in couplings:
         pattern = rf"(?<={arg}_)[\d]*.[\d]*"
         result = re.search(pattern, fname)
         if result is not None:
             res += f"{arg}_{result.group(0)}_"
+
+    # chem - we treat this differently because it is a vector for different flavors
+    pattern = r"chem_\[.*?\]"
+    result = re.search(pattern, fname)
+    if result is not None:
+        res += f"{arg}_{result.group(0)}_"
     return res
 
 
