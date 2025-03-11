6d0919c3081e3cc32667267b02b1373b0c6c90ce
Itay <itay.gomelski@mail.huji.ac.il>
2025-03-11 10:32:51 +0200

    Add the D6 config to the ggpeps.system package

--- src/ggpeps/system/__init__.py
+++ src/ggpeps/system/__init__.py
@@ -4,6 +4,8 @@ __all__ = [
     "system_z2_2d_2c",
     "system_z2_2d_G2c_F2c",
     "system_z2_2d_G2c_F4c",
+    "system_z2_2d_8c",
+    "system_D6_2d",
 ]
 from ggpeps.system.system_u1_2d import U1System2DConfig, U1System2D
 from ggpeps.system.system_z2_2d_1c import Z2System2DConfig
@@ -11,6 +13,7 @@ from ggpeps.system.system_z2_2d_2c import Z2System2D2CConfig
 from ggpeps.system.system_z2_2d_G2c_F2c import Z2System2D_G2C_F2C_Config
 from ggpeps.system.system_z2_2d_G2c_F4c import Z2System2D_G2C_F4C_Config
 from ggpeps.system.system_z2_2d_8c import Z2System2D_8C_Config
+from ggpeps.system.system_D6_2d import D6System2D_Config
 
 # System2D
 from ggpeps.system.system_implementation import Z2System2D
@@ -29,4 +32,5 @@ SystemConfigType = Union[
     Z2System2D_G2C_F2C_Config,
     Z2System2D_G2C_F4C_Config,
     Z2System2D_8C_Config,
+    D6System2D_Config,
 ]
