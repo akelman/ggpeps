b572f3ab64b2dae0cb2f0de32102000cb9772b6d
Itay <itay.gomelski@mail.huji.ac.il>
2025-04-23 16:48:57 +0300

    Update the Z2 gauge to group elemnt instead of theta also for the function get_neutral_gauge_value

--- src/ggpeps/gauge.py
+++ src/ggpeps/gauge.py
@@ -19,7 +19,7 @@ class ZNGauge:
         return np.array([[np.exp(1.0j * theta)]])
 
     def get_neutral_gauge_value(self) -> float:
-        return 0.0
+        return np.array([[1.0]])
 
     def get_possible_gauge_values(self) -> np.ndarray:
         prefactor = 2.0 * np.pi / self.n
