8b1accd77f6b2c6d2292221a579792b5735bbeec
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2025-06-08 17:54:02 +0300

    Update formatting

--- src/ggpeps/system/system_implementation.py
+++ src/ggpeps/system/system_implementation.py
@@ -1,6 +1,5 @@
 import logging
 from pfapack import pfaffian as pf
-from typing import List
 
 import numpy as np
 from ggpeps import xnp as xnp
@@ -14,12 +13,11 @@ from ggpeps.system.global_funcs import *
 from .system_base import System2DBase
 from .system_base import calculate_lognorm_inc
 
-# from ggpeps.system.global_funcs import update_gauge_ind
-
 logger = logging.getLogger(ggpeps.LOGGER_NAME)
 
 
 ###################### Z2System2D ##########################
+
 
 class Z2System2D(System2DBase):
     """2D Z2 system GGPEPS ansatz with physical fermions.
@@ -27,7 +25,7 @@ class Z2System2D(System2DBase):
     Some general notes about conventions:
 
     Order of the paramvec: see the functions that create the symbolvec in the configs.
-        We split the real and the imaginary part of the parameters into independent variables.
+    We split the real and the imaginary part of the parameters into independent variables.
     Mode order of tmat: {p,l1,r1,d1,u1,l2,r2,d2,u2,l3,r3... and so on}.
     Mode order of gamma_dirac: {p,l1,r1,d1,u1,l2,r2,d2,u2,p_dag,l1_dag,r1_dag,u1_dag,d1_dag,l2_dat,r2_dag,u2_dag,d2_dag,l3,r3... and so on}.
     Mode order of gamma_maj: {p_1,p_2,l1_1,l1_2,r1_1,r1_2,d1_1,d1_2,u1_1,u1_2,l2_1,l2_2,r2_1,r2_2,d2_1,d2_2,u2_1,u2_2,l3_1,l3_2... and so on}.
@@ -175,7 +173,8 @@ class Z2System2D(System2DBase):
         """Update method that is called upon changing a gauge field.
         This method is central to the algorithm since it changes the gauged projectors
         and updates all incremental trackers of determinants and inverses.
-        The re-calculation of determinants and inverses for the norm would be prohibitively expensive.
+        The re-calculation of determinants and inverses for the norm would be
+        prohibitively expensive.
 
         This method overwrites an abstract method in System2DBase.
 
@@ -463,7 +462,8 @@ class Z2System2D(System2DBase):
 
     def _compute_mag_energy_op(self, use_trans_inv: bool = True):
         """Computation of the magnetic energy operator (w/o shift).
-        This operator is diagonal in the gauge field (group element) basis and can thus be computed easily.
+        This operator is diagonal in the gauge field (group element) basis and can thus
+        be computed easily.
 
         This method overwrites an abstract method in System2DBase.
 
@@ -490,8 +490,10 @@ class Z2System2D(System2DBase):
         return mag_energy_bare
 
     def _compute_int_energy_op_vec_and_grad(self):
-        """Calculate the energy and energy gradient due to the interaction of the physical fermions with the gauge fields.
-        Note: this function assumes that U = U^dagger, which is valid only for Z2. For other groups, the calculation will not be as simple.
+        """Calculate the energy and energy gradient due to the interaction of the
+        physical fermions with the gauge fields.
+        Note: this function assumes that U = U^dagger, which is valid only for Z2.
+        For other groups, the calculation will not be as simple.
 
         Returns:
             tuple: Tuple of (interaction energy for a single link, gradients)
