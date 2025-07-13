87ca0a605760c0f737fc80a8882b6ec0282bbe98
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2025-07-13 14:36:49 +0300

    Improve Evaluator.evaluate() docstring

--- src/ggpeps/evaluator.py
+++ src/ggpeps/evaluator.py
@@ -26,11 +26,12 @@ class Evaluator(ABC):
         """Simulate the system and return the results as a dictionary of observables
 
         Raises:
-            NotImplementedError: _description_
+            NotImplementedError: raised if the method is not implemented in the subclass.
 
         Returns:
             dict: Dictionary of observables
-                  Each key-val pair is of the form (obs: List) where List is a list of values for the observable for the simulated gauge configurations
+                  Each key-val pair is of the form (obs: List) where List is a list of
+                  values for the observable for the simulated gauge configurations
         """
         raise NotImplementedError(
             "This is an abstract method. Implement in child class please."
