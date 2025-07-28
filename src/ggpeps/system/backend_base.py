0c245de5b036e9e0f503a4640f4ae02540344965
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2025-07-28 14:56:43 +0300

    Remove el_grad_vec functions from backend

--- src/ggpeps/system/backend_base.py
+++ src/ggpeps/system/backend_base.py
@@ -23,30 +23,3 @@ class BackendBase(ABC):
     @abstractmethod
     def calculate_lognormvec(gamma_in_sys_vec, mat_d_vec, all_factors=False):
         raise NotImplementedError("This is an abstract method. Implement in child class please.")
-
-    @staticmethod
-    @abstractmethod
-    def compute_el_grad_vec(
-        lattice_size: int,
-        num_pg_layer: int,
-        num_fermionic_layer: int,
-        unitcell_size: int,
-        nvirtmodes_link: int,
-        nphysmodes_site: int,
-        symbolvec: tuple,
-        overall_factors,
-        idxarr_vec,
-        el_energy_vec,
-        mat_b_mod_vec,
-        gamma_in_sys_mod_vec,
-        covmat_out_virt_vec,
-        norm_mod_vec,
-        lognorm_default_vec,
-        wi_gamma_in_mod_inv_vec,
-        wi_gamma_out_mod_inv_vec,
-        mat_d_mod_inv_vec,
-        gamma_maj_sys_deriv_layvec_ucvec_symbvec,
-        grad_over_norm_vec,
-        zeroed_params,
-    ):
-        raise NotImplementedError("This is an abstract method. Implement in child class please.")
