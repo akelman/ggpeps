6838e64840b6830a8ad59fd3fe78eb7d99ade594
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2025-07-30 12:51:07 +0300

    Rename system files

--- src/ggpeps/system/__init__.py
+++ src/ggpeps/system/__init__.py
@@ -14,12 +14,12 @@ from ggpeps.system.config_Z2_2d_2c import Z2System2D2CConfig
 from ggpeps.system.config_Z2_2d_G2c_F2c import Z2System2D_G2C_F2C_Config
 from ggpeps.system.config_Z2_2d_G4c_F4c import Z2System2D_G4C_F4C_Config
 from ggpeps.system.config_Z2_2d_G8c_F8c import Z2System2D_G8C_F8C_Config
-from ggpeps.system.system_implementation import Z2System2D
+from ggpeps.system.system_Z2 import Z2System2D
 
 # U1
 from ggpeps.system.config_U1_2d import U1System2DConfig
-from ggpeps.system.system_u1_2d import U1System2D
+from ggpeps.system.system_U1 import U1System2D
 
 # Dn
 from ggpeps.system.config_D6_2d import D6System2D_Config
-from ggpeps.system.system_implementation_D2n import D2nSystem2D
+from ggpeps.system.system_D2n import D2nSystem2D
