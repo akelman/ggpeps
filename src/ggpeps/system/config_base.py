f492e04181bcd643fd47bee180e545e4ef7911c8
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2025-08-04 19:43:30 +0300

    Revert "Use backend array assign"
    This reverts commit 8a74c82e52c16f46bbb604538bd1b6c5379e6816.

--- src/ggpeps/system/config_base.py
+++ src/ggpeps/system/config_base.py
@@ -14,7 +14,6 @@ from ggpeps import xscipy as xscipy
 import ggpeps
 from ggpeps import gauge
 from ggpeps.lattice import Lattice2D
-from ggpeps.system.backend import backend
 
 logger = logging.getLogger(ggpeps.LOGGER_NAME)
 
@@ -241,7 +240,17 @@ class Config2DBase(ABC):
             self.zeroed_params = self.get_zeroed_params()
 
         for coord in self.zeroed_params:
-            mat = backend.array_assign(mat, coord, 0)
+            # We do not use the backend, because this function can accept numpy arrays even when using jax
+            # (since the paramvec is always a numpy array)
+            if isinstance(mat, np.ndarray):  # TODO: handle jax better
+                mat[coord] = 0
+            elif isinstance(mat, jnp.ndarray):
+                mat = mat.at[coord].set(0)
+            else:
+                raise TypeError(
+                    "Unsupported type for mat in enforce_parameter_conditions: "
+                    f"{type(mat)}. Expected np.ndarray or jnp.ndarray."
+                )
         return
 
     @abstractmethod
