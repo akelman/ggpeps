e1785790bc53c631aadf2da91f9027aec6b5efb8
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2026-01-08 11:35:26 +0200

    Refactor functionality that extracts arguments from strings (dir names) and formats it for passing as CL argument Add type info

--- tools/build_slurm.py
+++ tools/build_slurm.py
@@ -6,38 +6,53 @@ def name_from_path(prefix, path):
     return prefix + path
 
 
-def folder2arg(foldername, arg):
-    # handle gauge_fixing separately, since it is not a numeric argument
-    if arg == "gf":
-        pattern = rf"(?<={arg}_)([^_]+)"
-        result = re.search(pattern, foldername)
-        if result is not None:
-            if result.group(0) == "T":
-                return "--gauge_fixing"
-            elif result.group(0) == "F":
-                return ""  # relies on the fact that the default value for gauge_fixing is False
-            elif result.group(0) == "c":
-                return "--gauge_fixing -2"
-            else:
-                return f"--gauge_fixing {result.group(0)}"
-        else:
-            return ""
+def folder2arg(foldername: str, arg: str) -> list:
+    """Extract from a string formatted in the form arg1_val_val_arg2_val_val... the values
+    corresponding to 'arg', as a list."""
 
-    # allow for any number of numeric values (ints or floats) separated by underscores
-    pattern = rf"(?<={arg}_)(-?\d+(?:\.\d+)?(?:_-?\d+(?:\.\d+)?)*)"
+    if arg == "gf" or arg == "gauge_fixing":
+        # handle gauge_fixing separately, since it does not take a numeric value
+        pattern = rf"(?<={arg}_)([^_]+)"
+    else:
+        # allow for any number of numeric values (ints or floats) separated by underscores
+        pattern = rf"(?<={arg}_)(-?\d+(?:\.\d+)?(?:_-?\d+(?:\.\d+)?)*)"
 
     result = re.search(pattern, foldername)
 
     if result is not None:
-        vals = " ".join(result.group(1).split("_"))
-        return f"--{arg} {vals}"
+        vals = result.group(1).split("_")
     else:
+        vals = []
+    return vals
+
+
+def format_arg(folder: str, arg: str) -> str:
+    vals = folder2arg(folder, arg)
+    if len(vals) == 0:
         return ""
 
+    if arg == "gf":
+        if len(vals) != 1:
+            raise ValueError("Gauge fixing argument should have a single value")
+        val = vals[0]
+        if val == "T":
+            return "--gauge_fixing"
+        elif val == "F":
+            return ""  # relies on the fact that the default value for gauge_fixing is False
+        elif val == "c":
+            return "--gauge_fixing -2"
+        else:
+            return f"--gauge_fixing {val}"
 
-def format_slurmfile(slurmfile, name, folder, time, command, args, num_threads):
+    formatted = "--" + arg + " " + " ".join(vals)
+    return formatted
+
+
+def format_slurmfile(
+    slurmfile: str, name: str, folder: str, time: str, command: str, args: list, num_threads: str
+) -> str:
     for arg in args:
-        command += " " + folder2arg(folder, arg)
+        command += " " + format_arg(folder, arg)
     return slurmfile.format(jobname=name, time=time, command=command, ncpu=num_threads)
 
 
@@ -104,7 +119,7 @@ if __name__ == "__main__":
         dest="args",
         nargs="+",
         help="args to parse from filepath",
-        default=["el", "mag", "int", "mass"],
+        default=["g", "int", "mass", "chem"],
     )
     parser.add_argument(
         "-n",
