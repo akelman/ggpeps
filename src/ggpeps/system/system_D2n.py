08892681328e4be4aa971184152f9de58ca3c01c
Ariel Kelman <17321799+akelman@users.noreply.github.com>
2026-03-19 04:42:47 +0200

    Update Dn el grad to match Z2

--- src/ggpeps/system/system_D2n.py
+++ src/ggpeps/system/system_D2n.py
@@ -564,7 +564,12 @@ class D2nSystem2D(System2DBase):
         lognorm_default = xnp.sum(lognorm_default_vec)
 
         # Calculate the derivatives (wrt all non-zero parameters) of the modified covmat_out
-        # TODO: can mask these
+        shape = (nlayer, len(mod_link_inds), unitcell_size, len(symbolvec), k, k)
+        d_covmat_out_virt_vec = xnp.zeros(shape)
+
+        l, m, u, s = inds
+        # NOTE: from limited testing, it appears that masking these is not worth it, as that creates extra copies.
+        # But it could be worth it if some of the matmuls were moved out of here, to happen once per eval.
         # (nlayer, nmodlinks, mod_virt_dim, mod_virt_dim)
         prod_mod_norm_vec = mat_d_mod_inv_vec @ gamma_in_mod_inv_vec @ gamma_in_sys_mod_vec
         # (nlayer, nmodlinks, mod_virt_dim, link_dim), take only the last k columns
@@ -572,10 +577,6 @@ class D2nSystem2D(System2DBase):
         # (nlayer, nmodlinks, link_dim, mod_virt_dim), take only the last k rows
         b_times_diff_vec = mat_b_mod_vec[:, :, -k:, :] @ gamma_out_mod_inv_vec
 
-        shape = (nlayer, len(mod_link_inds), unitcell_size, len(symbolvec), k, k)
-        d_covmat_out_virt_vec = xnp.zeros(shape)
-
-        l, m, u, s = inds
         diffB = diff_times_b_vec[l, m]
         Bdiff = b_times_diff_vec[l, m]
 
@@ -585,12 +586,14 @@ class D2nSystem2D(System2DBase):
             + Bdiff @ xnp.swapaxes(d_mat_b_vec, -1, -2)[..., :, -k:]
             - Bdiff @ d_mat_d_vec @ diffB
         )
-
         d_covmat_out_virt_vec = backend.array_assign(d_covmat_out_virt_vec, (l, m, u, s), vals)
 
         # Calculate the modified norms
         norm_shape = (nlayer, len(mod_link_inds), unitcell_size, len(symbolvec))
         prod_vec = xnp.zeros(norm_shape)
+        # The following line takes >50% of the runtime of this function, but most of that is actually spent
+        # copying data due to the fancy indexing of prod_mod_norm_vec[l, m]
+        # TODO: optimize this (the main complication is dealing with the reshaped arrays after indexing)
         vals = utils.trace_of_product((d_mat_d_vec, prod_mod_norm_vec[l, m]))
         prod_vec = backend.array_assign(prod_vec, (l, m, u, s), vals)
 
