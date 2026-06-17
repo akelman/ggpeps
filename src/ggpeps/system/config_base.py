e003188b0d7cacc8c10a9b47d44e1ca274644df6
Itay Gomelski <itay.gomelski@mail.huji.ac.il>
2026-06-17 13:23:50 +0300

    Swap l and r modes in get_cov_matrix_idx

--- src/ggpeps/system/config_base.py
+++ src/ggpeps/system/config_base.py
@@ -753,6 +753,10 @@ def get_cov_matrix_idx(
     Returns:
         int: index in the covariance matrix - 0-based
     """
+    if direction == 1:
+        direction = 2
+    else:
+        direction = 1
     idx = (
         (color - 1) * (ncopies * ndirections * 2)
         + (copy - 1) * (ndirections * 2)
